f064fcf078802b834589a2b2887357f607e4d95a
Mr-Ducklang <136151387+Mr-Ducklang@users.noreply.github.com>
2024-11-01 10:39:09 +1100

    Added templates like inventory, greeting and created test file as well

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template
 
 
-
 app = Flask(__name__)
 
-
-
 @app.route('/basic')
-
 def basic():
+    return render_template('basic.html')
 
-    return render_template('base.html')
+@app.route('/greet/<name>')
+def greet(name):
+    return render_template('greet.html', name=name)
 
+@app.route('/inventory')
 
+def inventory():
+
+    inventory_items = ['banana', 'cherry', '1990 Mazda MX-5','100,000kg of uranium', 'toothpaste']
+
+    return render_template('inventory.html', inventory=inventory_items)
 
 if __name__ == '__main__':
-
     app.run(debug=True)
