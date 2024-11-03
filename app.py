ea83ebbe7bf683c7089efddee3b6775b9725a43d
Mr-Ducklang <136151387+Mr-Ducklang@users.noreply.github.com>
2024-11-04 09:47:27 +1100

    Added profile pic, profile.html, and style.css, completed task 3

--- app.py
+++ app.py
@@ -11,13 +11,27 @@ def basic():
 def greet(name):
     return render_template('greet.html', name=name)
 
-@app.route('/inventory')
 
+
+@app.route('/inventory')
 def inventory():
 
-    inventory_items = ['banana', 'cherry', '1990 Mazda MX-5','100,000kg of uranium', 'toothpaste']
+    inventory_items = ['banana', 'cherry', '1990 Mazda MX-5','100,000kg of uranium', 'toothpaste', 'apple']
 
     return render_template('inventory.html', inventory=inventory_items)
+
+@app.route('/test/<message>')
+
+def test(message):
+
+    return render_template('test.html', message=message)
+
+
+@app.route('/profile/<username>')
+
+def profile(username):
+
+    return render_template('profile.html', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
