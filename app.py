7b7647e4273c5cf9cb70468a62cc912123275a54
Mr-Ducklang <136151387+Mr-Ducklang@users.noreply.github.com>
2024-11-05 11:48:40 +1100

    added thankyou, a new base file,

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
@@ -28,10 +28,37 @@ def test(message):
 
 
 @app.route('/profile/<username>')
-
 def profile(username):
-
     return render_template('profile.html', username=username)
+
+#GET Requests data from server and POST submits data to server
+@app.route('/contact', methods=['GET', 'POST'])
+
+def contact():
+    #checks if method is post request
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        # Here you would typically save this data or send an email
+        #after processing form data, redirects to thank you page here
+        return redirect(url_for('thankyou', name=name, message=message))
+
+    return render_template('contact.html')
+
+
+
+@app.route('/thankyou')
+
+def thankyou():
+
+    name = request.args.get('name')
+
+    message = request.args.get('message')
+
+    return render_template('thankyou.html', name=name, message=message)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
